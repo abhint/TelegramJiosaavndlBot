a6087e1dafcbde699b98d5a379f4882bfe2f8c25
Abhijith N T <abhijithnt64@gmail.com>
2021-05-24 22:29:21 +0530

    Delete dist directory

--- dist/time.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function minutesC(seconds) {
-    let minutes = Math.floor(seconds / 60);
-    let second = Math.floor(seconds % 60);
-    return `${minutes}:${second}`;
-}
-exports.default = minutesC;
--- dist/utils/message.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const message = {
-    start: "<b>Telegram is an Inline bot. Search for songs And help you to download.</b>\n"
-};
-exports.default = message;
