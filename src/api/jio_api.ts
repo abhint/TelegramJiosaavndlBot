0bc91896682ee537b357589daf2dc117a755b6ed
AbhijithNT <abhijithnt64@gmail.com>
2021-06-12 22:57:05 +0530

    :white_check_mark: (removed comments)

--- src/api/jio_api.ts
+++ src/api/jio_api.ts
@@ -1,11 +1,10 @@
-//
 export function songSearch(songname: string): string {
   return `https://www.jiosaavn.com/api.php?__call=search.getResults&_format=json&p=1&_marker=0&ctx=android&q=${songname}`;
 }
-//
-export function jioSongDownload(dlLink: string) {
-  return dlLink
+
+export function jioSongDownload(preview_url: string) {
+  return preview_url
     .replace("preview.saavncdn.com", "jiosaavn.cdn.jio.com")
     .replace("_96_p", "_320")
-    .replace(".mp4", ".mp3")
+    .replace(".mp4", ".mp3");
 }
