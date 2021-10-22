ba5c3b3afbf078c9dc1139e92621c6c8f7218d61
AbhijithNT <abhijithnt64@gmail.com>
2021-10-22 21:27:40 +0530

    get api result

---
+++ bot/utils/connection_handlers.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { songSearch, jioSongDownload } from "../others/endpoints";
+
+// interface songs {
+//   id: string;
+//   song: string;
+//   media_preview_url: string;
+//   duration: string;
+// }
+
+export async function get_result(query: string) {
+  let url = songSearch(query);
+  let response = await (await axios.get(url)).data;
+  return sortAPI(response.results);
+}
+
+async function sortAPI(result: any) {
+  //   console.log(result);
+  let songs_details: Array<object> = [];
+  for (let get in result) {
+    songs_details.push({
+      type: "audio",
+      id: result[get].id,
+      audio_url: result[get].media_preview_url,
+      title: result[get].song,
+      parse_mode: "html",
+      audio_duration: result[get].duration,
+      caption: `<b>Name: ${result[get].song}\nYear: ${result[get].year}\nLanguage: ${result[get].language}</b>`,
+    });
+  }
+  console.log(songs_details);
+  
+}
+
