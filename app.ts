8dd251daa360c006b9f2d045d800541dbc0f7648
AbhijithNT <abhijithnt64@gmail.com>
2021-10-22 21:47:45 +0530

    inline

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import bot from "./bot/bot";
 import { command } from "./bot/commands";
+import { onInline } from "./bot/inline";
 (async () => {
+  await onInline();
   await command();
   await bot.launch();
 })();
