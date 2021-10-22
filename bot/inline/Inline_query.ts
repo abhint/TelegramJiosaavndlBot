1052d01edbee0d5dcc01fae42d9307270cefdfc5
AbhijithNT <abhijithnt64@gmail.com>
2021-10-22 21:39:32 +0530

    export

--- bot/inline/Inline_query.ts
+++ bot/inline/Inline_query.ts
@@ -1,7 +1,7 @@
 import { Composer } from "telegraf";
 import { get_result } from "../utils/connection_handlers";
 
-const onInlineQuery = Composer.on("inline_query", async (msg) => {
+export const onInlineQuery = Composer.on("inline_query", async (msg) => {
   const query: string = msg.update.inline_query.query;
   let result: any = await get_result(query);
   await msg.answerInlineQuery(result);
