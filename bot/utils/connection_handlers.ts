5f90853ca336a198224b2651c31548652ea003db
AbhijithNT <abhijithnt64@gmail.com>
2021-10-23 21:48:39 +0530

    map()

--- bot/utils/connection_handlers.ts
+++ bot/utils/connection_handlers.ts
@@ -1,33 +1,22 @@
 import axios from "axios";
-import { songSearch, jioSongDownload } from "../others/endpoints";
-
-// interface songs {
-//   id: string;
-//   song: string;
-//   media_preview_url: string;
-//   duration: string;
-// }
+import { songSearch } from "../others/endpoints";
+import { v4 as uuidv4 } from "uuid";
 
 export async function get_result(query: string) {
   let url = songSearch(query);
-  let response = await (await axios.get(url)).data;
-  return sortAPI(response.results);
-}
-
-async function sortAPI(result: any) {
-
-  let songs_details: Array<object> = [];
-  for (let get in result) {
-    songs_details.push({
+  let response = (await axios.get(url)).data;
+  let response_results = response.results;
+  let results = response_results
+    // @ts-ignore
+    .map(({ media_preview_url, song, duration, year, language }) => ({
       type: "audio",
-      id: result[get].id,
-      audio_url: result[get].media_preview_url,
-      title: result[get].song,
+      id: uuidv4(),
+      audio_url: media_preview_url,
+      title: song,
+      audio_duration: Number(duration),
       parse_mode: "html",
-      audio_duration: Number(result[get].duration),
-      caption: `<b>Name: ${result[get].song}\nYear: ${result[get].year}\nLanguage: ${result[get].language}</b>`,
-    });
-  }
-  console.log(songs_details);
-  return songs_details;
+      caption: `<b>Name: ${song}\nYear: ${year}\nLanguage: ${language}</b>`,
+    }));
+
+  return results;
 }
