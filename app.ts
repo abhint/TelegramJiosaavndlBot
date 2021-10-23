7b8587d73d3f678ee29ff34c9ff18fa3439e8063
AbhijithNT <abhijithnt64@gmail.com>
2021-10-23 21:47:25 +0530

    webhook()

--- app.ts
+++ app.ts
@@ -4,5 +4,11 @@ import { onInline } from "./bot/inline";
 (async () => {
   await onInline();
   await command();
-  await bot.launch();
+  // await bot.launch();
+  await bot.launch({
+    webhook: {
+      domain: String(process.env.URL),
+      port: Number(process.env.PORT),
+    },
+  });
 })();
